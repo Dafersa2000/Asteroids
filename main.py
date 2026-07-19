447e5bd44ec5902300d9ec63bd63c178ee51ea42
dafersa2000 <da.fernandez16@hotmail.com>
2026-07-18 21:21:59 -0500

    First Screen

--- main.py
+++ main.py
@@ -3,9 +3,19 @@ from logger import log_state
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT
 
 def main():
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))
     print("Starting Asteroids")
     print(rf"Screen width: {SCREEN_WIDTH}")
     print(rf"Screen height: {SCREEN_HEIGHT}")
+    while True:
+        log_state()
+        screen.fill(color = (0,0,0))
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        pygame.display.flip()
+
 
 
 if __name__ == "__main__":
